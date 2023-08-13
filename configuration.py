e528c032a26d788496d763f6d09ce593317b3af6
Vitaly Yurchenko <121400984+Vitaly-Yurchenko@users.noreply.github.com>
2023-08-13 11:21:42 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://0694ad42-3a3c-4a4f-a984-f0567e3877ac.serverhub.praktikum-services.ru/"
+CREATE_NEW_ORDER = "/api/v1/orders"
+GET_ORDER_BY_NUMBER = "/api/v1/orders/track?t={track_number}"
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+        "firstName": "АА",
+        "lastName": "ЯЯ",
+        "address": "Санкт",
+        "metroStation": 204,
+        "phone": "+79111111111",
+        "rentTime": 5,
+        "deliveryDate": "2023-08-14",
+        "comment": "Привет",
+        "color": ["BLACK"]
+}
+
+headers = {'Content-Type': 'application/json'}
